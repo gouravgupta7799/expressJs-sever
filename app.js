6d2d818d60265e8ed7977b6fd459e00bd61d48d6
Gourav Gupta <gouravgupta7799@gmail.com>
2023-03-10 22:32:53 +0530

    form added with input tag

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 let express = require("express");
 let app = express();
+let bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended:false}));
 
-app.use((req, res, next) => {
-  console.log("hello, this is middleware fisrt")
-  next()
+app.use("/add-product",(req, res, next) => {
+  // console.log("this is middleware secound");
+  res.send(`<html>
+  <body>
+  <form action="/product" method="POST">
+  <input type="text" name="title">
+  <input type="text" name="product-size">
+  <button type="submit">click</button>
+  </form>
+  </body>
+  </html>`);
 })
 
-// app.use((req, res, next) => {
-//   console.log("this is middleware secound")
-//   res.send("<h1>hello</h1>")
-//   next()
-// })
-app.use((req, res, next) => {
-  console.log("this is middleware secound")
-  res.send({ "key1": 2 })
+app.use("/product", (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+});
+
+app.use("/",(req, res, next) => {
+  // console.log("hello, this is middleware fisrt");
+  res.send("<h1>hello, this is middleware first</h1>");
 })
 
 app.listen(4000);
